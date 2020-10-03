6c201f7b9173f315c0468a05d77b9f5a461e06c3
Satyam-Bharadwaj <satyam.bharadwaj.96@gmail.com>
2020-10-03 20:33:49 +0530

    Toy mode of url shortner

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser')
 var cors = require('cors');
 
 var app = express();
-
+//bodyParser Configuration
+app.use(bodyParser.urlencoded({extended: false}));
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
@@ -30,7 +31,18 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+let data = [];
+let dataSize=0;
+app.post("/api/shorturl/new",(req,res)=>{
+data.push(req.body.url);
+dataSize=dataSize+1;
+res.send({original_url:req.body.url,short_url:dataSize-1});
+});
 
+app.get("/api/shorturl/:short_url",(req,res)=>{
+  var src_url = data[req.params.short_url];
+  res.redirect(src_url);
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
